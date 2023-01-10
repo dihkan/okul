5fb9aa61b067ed67dc5145bfadf292c53744bce0
dihkan <dihkan@gmail.com>
2023-01-10 12:21:01 +0300

    Klasör işlemleri

---
+++ dene/kayit.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_POST) {
+    $conn = new mysqli("localhost", "root", "", "uyeler");
+    $uyeAdi = $_POST['uyeAdi'];
+    $sifre = $_POST['sifre'];
+
+    $sql = "insert into uyeler (uyeAdi , sifre) VALUES ('" . $uyeAdi . "' , '" . $sifre . "')";
+
+    $conn->query($sql);
+} else {
+    echo "Veri yok";
+}
---
+++ klasorIslemleri/sil.php
@@ -0,0 +1,21 @@
+<?php
+    if($_POST['delete']){
+        $b = new mysqli("localhost", "root" , "","okul");
+        $userId = $_POST['userId'];
+        
+        $sec = $b->query("SELECT * FROM users WHERE userId='$userId'")->fetch_array(MYSQLI_ASSOC);
+
+        $silinecekDosya = $sec['photo'];
+        
+        if(file_exists($_SERVER['DOCUMENT_ROOT'].'/okul/klasorIslemleri/upload/' . $silinecekDosya)){
+            unlink($_SERVER['DOCUMENT_ROOT'].'/okul/klasorIslemleri/upload/'. $silinecekDosya);
+        }
+        $sql = "DELETE from users where userId = '$userId'";
+        $b->query($sql);
+        echo '<div class="msg-info">User deleted successfully.</div>';
+
+
+    }
+    header("Refresh:2;url=users.php");
+
+?>
